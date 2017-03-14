ae3d07ce6f7b94b9d829ee7ccdaf96833e67e8ed
F1 race car team <ucichemecars@gmail.com>
2017-03-14 16:56:40 -0700

    Added run Time and rotation count

--- FreeRun/FreeRun.ino
+++ FreeRun/FreeRun.ino
@@ -1,7 +1,11 @@
 #include "DualMC33926MotorShield.h"
-
+#include "DualMC33926MotorShield.h"
+#include <PololuWheelEncoders.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
 DualMC33926MotorShield md;
-
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
 void stopIfFault()
 {
   if (md.getFault())
@@ -16,10 +20,53 @@ void setup()
   Serial.begin(115200);
   Serial.println("Dual MC33926 Motor Shield");
   md.init();
+   PololuWheelEncoders::init(3,5,6,11);
 }
 
 void loop()
 {
    md.setM1Speed(380);
    md.setM2Speed(400);
+   static float total2=0;
+  static float total1=0;
+  static int flag1=0;
+  static int flag2=0;
+  static int flag3=0;
+  static int flag4=0;
+  static int flag5=0;
+  static int flag6=0;
+  static int speed1;
+  static int speed2;
+  static long int runTime=0;
+  static long int startTime=0;
+  static int co;
+  static signed int adjust=1;//adjustment to the speed everytime the loop is run
+  static int counter=0;//count the total times the loop has been run
+  static int lux;
+  static int preLux=0;
+  static long int count=0;
+  static long timer1=0;//Counte until max is reached
+  static long timer2=0;//Count until lux 30 is reached
+  //static long testtime=27408;
+  static long currentT;
+  static float tagertRotate=1000;
+    static int mode=0;
+    static int t1=millis(); //t1 is the time it starts
+  int j=PololuWheelEncoders::getCountsAndResetM2();
+  int i=PololuWheelEncoders::getCountsAndResetM1();
+  total2=total2+abs(j/3591.84);
+  total1=total1+abs(i/3591.84);
+  if(total1>0.01 && flag6==0)
+  {
+    startTime=millis();
+    flag6=1;
+  }
+  if(flag6==1)
+  {
+    runTime=millis()-startTime;
+    Serial.print(runTime);
+    Serial.print("      ");
+  }
+  Serial.print(total1); Serial.print("    ");
+  Serial.println(total2);
 }
